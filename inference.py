e094feb951280ee308a7b51dc65b76c652d64fb4
Alejandro Vidaurrazaga <Avidaurrazagaiturmendi@gmail.com>
2021-09-01 20:42:10 +0300

    testing receive image

--- inference.py
+++ inference.py
@@ -1,5 +1,15 @@
 import pickle
-
-# file_to_read = open('Logistic.pkl', 'rb')
-file_to_read = open('Random_Forest.pkl', 'rb')
+#
+file_to_read = open('Logistic.pkl', 'rb')
+# file_to_read = open('Random_Forest.pkl', 'rb')
 clf = pickle.loads(file_to_read.read())
+# import pandas as pd
+# from matplotlib import pyplot as plt
+#
+# df = pd.read_csv('emnist-letters-train.csv')
+# y = df.iloc[:, 1]
+# X = df.iloc[:, 1:]
+#
+# plt.imshow(X.iloc[69, :].T.to_numpy().reshape(28, 28), cmap='gray')
+# print(y[69])
+# plt.show()
