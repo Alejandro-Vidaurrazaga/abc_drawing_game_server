c198d54d2d21c52f12b18350efc7cf93b7b8e073
Alejandro Vidaurrazaga <Avidaurrazagaiturmendi@gmail.com>
2021-09-01 18:56:35 +0300

    testing receive image

--- inference.py
+++ inference.py
@@ -1,4 +1,5 @@
 import pickle
 
-file_to_read = open('Logistic.pkl', 'rb')
+# file_to_read = open('Logistic.pkl', 'rb')
+file_to_read = open('Random_Forest.pkl', 'rb')
 clf = pickle.loads(file_to_read.read())
