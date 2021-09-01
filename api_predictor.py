b91dcb25d6e1e9237b8ec445b4ade1d3f4b951d7
Alejandro Vidaurrazaga <Avidaurrazagaiturmendi@gmail.com>
2021-09-01 15:05:07 +0300

    testing receive image

--- api_predictor.py
+++ api_predictor.py
@@ -29,7 +29,7 @@ def good_prediction():
 def bad_prediction():
     if request.is_json:
         req = request.get_json()
-        return make_response(jsonify({'key': 'Im working'}), 200)
+        return make_response(jsonify(req), 200)
 
     return make_response(jsonify({'predicted_letter': 'B', 'certain': 0.51, 'real_letter': 'A'}, 200))
 
