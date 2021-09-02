16d0fb6f7a36fc7a7526f1b23e083ef9c547b78b
Alejandro Vidaurrazaga <Avidaurrazagaiturmendi@gmail.com>
2021-09-02 14:04:47 +0300

    testing receive image3

--- api_predictor.py
+++ api_predictor.py
@@ -1,7 +1,14 @@
+import base64
+import io
 import os
+import string
+import numpy as np
+from PIL.Image import Image
 from flask_cors import CORS, cross_origin
 from flask import Flask, request, make_response, jsonify
-
+from skimage.color import rgb2gray, rgba2rgb
+from skimage.transform import resize
+from inference import clf
 
 app = Flask(__name__)
 # cors = CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers='*')
@@ -39,38 +46,37 @@ def bad_prediction():
     return make_response(jsonify({'predicted_letter': 'B', 'certain': 0.51, 'real_letter': 'A'}, 200))
 
 
-# def fix_input_image(str_img):
-#     image = Image.open(io.BytesIO(base64.urlsafe_b64decode(str_img)))
-#     image_np = np.array(image)
-#     image_np = rgb2gray(rgba2rgb(image_np))
-#     image = resize(image_np, (28, 28))
-#
-#     squarer2 = lambda t: 0 if t == 255 else t
-#     squarer = lambda t: (int((t + 1) * 255 / 2) - 255) * -1
-#     vfunc = np.vectorize(squarer)
-#     image = vfunc(image)
-#     vfunc = np.vectorize(squarer2)
-#     image = vfunc(image)
-#
-#     plt.imshow(image, cmap='gray')
-#     print(image)
-#     # plt.imshow(zeros.astype(int), cmap='gray')
-#     plt.show()
-#
-#     print(clf.predict(image.reshape((1, -1))))
-#     print(clf.predict_proba(image.reshape((1, -1))))
+def fix_input_image(str_img):
+    image = Image.open(io.BytesIO(base64.urlsafe_b64decode(str_img)))
+    image_np = np.array(image)
+    image_np = rgb2gray(rgba2rgb(image_np))
+    image = resize(image_np, (28, 28))
+
+    squarer2 = lambda t: 0 if t == 255 else t
+    squarer = lambda t: (int((t + 1) * 255 / 2) - 255) * -1
+    vfunc = np.vectorize(squarer)
+    image = vfunc(image)
+    vfunc = np.vectorize(squarer2)
+    image = vfunc(image)
+
+    dic_letters = {i: letter for i, letter in enumerate(string.ascii_uppercase)}
+    row = image.reshape((1, -1))
+
+    return dic_letters[clf.predict(row)[0]], clf.predict_proba(np.max(row))
 
 
 @app.route("/json", methods=["POST"])
 @cross_origin(supports_credentials=True)
 def json():
     if request.is_json:
-        req = request.get_json()
         try:
-            return make_response(jsonify({'letter': 'This is your letter'}), 200)
+            req = request.get_json()
+            letter, certain = fix_input_image(req.get("data"))
+
+            return make_response(jsonify({'letter': letter, 'certain': certain}), 200)
         except Exception as ex:
             print(ex)
-        return make_response(jsonify({'error': 'error'}), 400)
+        return make_response(jsonify({'error': ex}), 400)
     else:
         return make_response(jsonify({"message": "No JSON"}), 400)
 
--- inference.py
+++ inference.py
@@ -1,4 +1,4 @@
 import pickle
 
-# file_to_read = open('Logistic.pkl', 'rb')
-# clf = pickle.loads(file_to_read.read())
+file_to_read = open('Logistic.pkl', 'rb')
+clf = pickle.loads(file_to_read.read())
